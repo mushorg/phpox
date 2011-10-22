0c0b700334acddcecd1fe58ed55c9991b4d96c50
Louis Liu <louisliu@locomotion.tw>
2011-10-22 14:56:41 +0000

    1. Imporved Makefile 2. Add supporting of "cat /proc/cpuinfo" to system().

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 all: apd_sandbox.php
 
-apd_sandbox.php: apd_generate.py
+apd_sandbox.php: apd_generate.py replacement/*.py
 	python apd_generate.py > apd_sandbox.php
+clean:
+	rm apd_sandbox.php
--- replacement/system.py
+++ replacement/system.py
@@ -2,11 +2,28 @@ def call():
     # TODO: Make uptime dynamic
     function = """\tif ($cmd == 'id') {
 \t\t$ret = array('uid=0(root) gid=0(root) groups=0(root)',);
-\t}
-\telseif ($cmd == 'uptime') {
+\t}elseif ($cmd == 'uptime') {
 \t\t$ret = array('16:12:55 up 152 days, 19:03,  0 user,  load average: 0.02, 0.02, 0.03',);
-\t}
-\telse {
+\t}\n\telseif( $cmd == 'cat /proc/cpuinfo') { \n
+\t $ret = array('processor	: 0
+cpu		: TI UltraSparc III+ (Cheetah+)
+fpu		: UltraSparc III+ integrated FPU
+pmu		: ultra3+
+prom		: OBP 4.5.21 2003/02/24 17:23
+type		: sun4u
+ncpus probed	: 2
+ncpus active	: 2
+D$ parity tl1	: 0
+I$ parity tl1	: 0
+Cpu0ClkTck	: 000000003c7fabc0
+Cpu1ClkTck	: 000000003c7fabc0
+MMU Type	: Cheetah+
+State:
+CPU0:		online
+CPU1:		online
+', 0);
+\t}else {
 \t\t$ret = array('None',);
-\t}"""
+\t}
+\t return $ret;"""
     return function
