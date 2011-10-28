78bcfbb9032b1b5fcf5e77bcf7e3c30f00e71668
Alice Yang <yanggh312@gmail.com>
2011-10-28 15:01:23 +0000

    added columns by Alice

--- log_sqlite.py
+++ log_sqlite.py
@@ -12,9 +12,11 @@ class LogSQLite(object):
                 CREATE TABLE IF NOT EXISTS 
                 events(id INTEGER PRIMARY KEY, analysis_date TEXT, file_md5 TEXT, 
                 file_name TEXT, irc_addr INTEGER, irc_server_pwd TEXT, irc_nick TEXT,
-                irc_user TEXT, irc_mode TEXT, irc_channel TEXT)""")
+                irc_user TEXT, irc_mode TEXT, irc_channel TEXT, irc_nickserv TEXT,
+                irc_notice TEXT, irc_privmsg TEXT)""")
         self.connection.commit()
         self.cursor.close()
+        self.connection.close()
         
     def insert(self, botnet):
         self.cursor = self.connection.cursor()
@@ -22,6 +24,8 @@ class LogSQLite(object):
                 INSERT INTO events VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", 
                 (None, botnet.analysis_date, botnet.file_md5, botnet.file_name, 
                  botnet.irc_addr, botnet.irc_server_pwd, botnet.irc_nick,
-                 botnet.irc_user, botnet.irc_mode, ', '.join(botnet.irc_channel)))
+                 botnet.irc_user, botnet.irc_mode, ', '.join(botnet.irc_channel),
+                 botnet.irc_nickserv, ', '.join(botnet.irc_notice), ', '.join(botnet.irc_privmsg)))
         self.connection.commit()
         self.cursor.close()
+        self.connection.close()
