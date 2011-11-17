5dcabcfa58406f47ce0a636ac58179e20335fe1a
Alice Yang <yanggh312@gmail.com>
2011-11-17 05:59:29 +0000

    change column's format

--- alter_table.py
+++ alter_table.py
@@ -11,9 +11,7 @@ class LogSQLite(object):
         '''self.cursor.execute("""ALTER TABLE events ADD COLUMN 'irc_nickserv TEXT'""")
         self.cursor.execute("""ALTER TABLE events ADD COLUMN 'irc_notice TEXT'""")
         self.cursor.execute("""ALTER TABLE events ADD COLUMN 'irc_privmsg TEXT'""")'''
-        #first_analysis:0/1->haven't analyzed/analyzed ; last_analysis:last analysis date
-        #last_analysis question: Is TEXT OK? 
-        self.cursor.execute("""ALTER TABLE events ADD COLUMN 'first_analysis INTEGER'""")
+        self.cursor.execute("""ALTER TABLE events ADD COLUMN 'first_analysis TEXT'""")
         self.cursor.execute("""ALTER TABLE events ADD COLUMN 'last_analysis TEXT'""")
         self.connection.commit()
         self.cursor.close()
