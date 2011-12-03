32f06fb7af2d78b8b480f6f7bba97d3a349ad55a
Julia Cheng <julia.yc.cheng@gmail.com>
2011-12-03 11:07:23 +0000

    momo update

--- replacement/dl.py
+++ replacement/dl.py
@@ -1,8 +1,5 @@
 def call():
     function = """ if (!extension_loaded('apd')) {
-        \t{return 'NO Newly Version;};
-        \t} else {
-           \t dl('apd.so');
-        \t{return FALSE;}}""" 
+        \t{return FALSE;};}""" 
     return function    
 #print call()
