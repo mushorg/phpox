48a08ee451e24f8c25192a45fd2e498b104ac7ca
Guesslin <guesslin1986@gmail.com>
2011-12-06 10:12:18 +0000

    Start Web_interface with submit.py

---
+++ web_interface/submit.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
+
+print "Content-type: text/html"
+print
+
+print """
+<html>
+
+<head><title>Sample CGI Script</title></head>
+
+<body>
+
+  <h3> Sample CGI Script </h3>
+"""
+
+form = cgi.FieldStorage()
+message = form.getvalue("upload_file", "(no file uploaded")
+
+print """
+
+  <p>Previous message: %s</p>
+
+  <p>form
+
+  <form method="post" action="submit.py">
+    <p>message: <input type="file" name="upload_file"/></p>
+  </form>
+
+</body>
+
+</html>
+""" % len(cgi.escape(message))
