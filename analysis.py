692755b8328f090474ac3d53eee10c496e61ae6b
Louis Liu <louisliu@locomotion.tw>
2011-12-06 13:00:11 +0000

    add toxml() method to Botnet object.

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import hashlib
 from datetime import datetime
+from xml.dom.minidom import Document
 
 class Botnet(object):
     
@@ -16,6 +17,65 @@ class Botnet(object):
         self.irc_nickserv = ""
         self.irc_notice = []
         self.irc_privmsg = []
+
+    def toxml(self):
+        doc = Document()
+        xml = doc.createElement('xml')
+        doc.appendChild(xml)
+        bot = doc.createElement('bot')
+        xml.appendChild(bot)
+        host = doc.createElement('host')
+        bot.appendChild(host)
+        if( len(self.irc_addr) > 0):
+            host.appendChild(doc.createTextNode(self.irc_addr))
+        irc_server_pwd = doc.createElement('irc_server_pwd')
+        bot.appendChild(irc_server_pwd)
+        if( len(self.irc_server_pwd) > 0):
+            irc_server_pwd.appendChild(doc.createTextNode(self.irc_server_pwd))
+        irc_nick = doc.createElement('irc_nick')
+        bot.appendChild(irc_nick)
+        if( len(self.irc_nick) > 0):
+            irc_nick.appendChild(doc.createTextNode(self.irc_nick))
+        irc_user = doc.createElement('irc_user')
+        bot.appendChild(irc_user)
+        if( len(self.irc_user) > 0):
+            irc_user.appendChild(doc.createTextNode(self.irc_user))
+        irc_mode = doc.createElement('irc_mode')
+        bot.appendChild(irc_mode)
+        if(len(self.irc_mode)>0):
+            irc_mode.appendChild(doc.createTextNode(self.irc_mode))
+        irc_nickserv = doc.createElement('irc_nickserv')
+        bot.appendChild(irc_nickserv)
+        if(len(self.irc_nickserv)> 0):
+            irc_nickserv.appendChild(doc.createTextNode(self.irc_nickserv))
+        irc_channels = doc.createElement('irc_channels')
+        bot.appendChild(irc_channels)
+        if( len(self.irc_channel) > 0):
+            for i in self.irc_channel:
+                irc_channel = doc.createElement('irc_channel')
+                irc_channels.appendChild(irc_channel)
+                irc_channel.appendChild(doc.createTextNode(i))
+        else:
+            irc_channels.appendChild(doc.createElement('irc_channel'))
+        irc_notices = doc.createElement('irc_notices')
+        bot.appendChild(irc_notices)
+        if( len(self.irc_notice) > 0):
+            for i in self.irc_notice:
+                irc_notice = doc.createElement('irc_notice')
+                irc_notices.appendChild(irc_notice)
+                irc_notice.appendChild(doc.createTextNode(i))
+        else:
+            irc_notices.appendChild(doc.createElement('irc_notice'))
+        irc_privmsgs = doc.createElement('irc_privmsgs')
+        bot.appendChild(irc_privmsgs)
+        if( len(self.irc_privmsg) > 0):
+            for i in self.irc_privmsg:
+                irc_privmsg = doc.createElement('irc_privmsg')
+                irc_privmsgs.appendChild(irc_privmsg)
+                irc_privmsg.appendChild(doc.createTextNode(i))
+        else:
+            irc_privmsgs.appendChild(doc.createElement('irc_privmsg'))
+        return xml.toprettyxml(indent="" )
 
 class DataAnalysis(object):
 
