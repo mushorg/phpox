a7a36f86f82382f84b8d1b20428df3e8697119fb
Julia Cheng <julia.yc.cheng@gmail.com>
2011-12-08 06:34:27 +0000

    uncomment the """ """ in alter() function

--- alter_table.py
+++ alter_table.py
@@ -1,5 +1,14 @@
+"""
+It's used to extend the database, sandbox.db with text COLUMN 
+"irc_nickserv", "irc_notice", "irc_privmsg", and "first_analysis."
+"""
+
 import sqlite3
 
+"""
+class LogSQLite with member function ¡§alter¡¨ which will add columns to 
+the database , "sandbox.db" , and declare an instance of class LogSQLite.
+"""
 class LogSQLite(object):
     
     def __init__(self):
@@ -8,9 +17,9 @@ class LogSQLite(object):
         
     def alter(self):
         self.cursor = self.connection.cursor()
-        '''self.cursor.execute("""ALTER TABLE events ADD COLUMN 'irc_nickserv TEXT'""")
+        self.cursor.execute("""ALTER TABLE events ADD COLUMN 'irc_nickserv TEXT'""")
         self.cursor.execute("""ALTER TABLE events ADD COLUMN 'irc_notice TEXT'""")
-        self.cursor.execute("""ALTER TABLE events ADD COLUMN 'irc_privmsg TEXT'""")'''
+        self.cursor.execute("""ALTER TABLE events ADD COLUMN 'irc_privmsg TEXT'""")
         self.cursor.execute("""ALTER TABLE events ADD COLUMN 'first_analysis TEXT'""")
         self.cursor.execute("""ALTER TABLE events ADD COLUMN 'last_analysis TEXT'""")
         self.connection.commit()
