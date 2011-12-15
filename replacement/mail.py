60aafc3609be0530291a79cdd11aa916bcfe4bf5
Julia Cheng <julia.yc.cheng@gmail.com>
2011-12-15 17:07:45 +0000

    mail update

--- replacement/mail.py
+++ replacement/mail.py
@@ -1,17 +1,13 @@
-from email.parser import Parser
+#from email.parser import Parser
 
-headers = Parser().parsestr('From: <user@example.com>\n'
-        'To: <someone_else@example.com>\n'
-        'Subject: Test message\n'
-        'Message: Send Successfully\n')
+#headers = Parser().parsestr('From: <user@example.com>\n'
+        #'To: <someone_else@example.com>\n'
+        #'Subject: Test message\n'
+        #'Message: Send Successfully\n')
 
-#print 'To: %s' % headers['to']
-#print 'From: %s' % headers['from']
-#print 'Subject: %s' % headers['subject']
-#print 'message: %s' % headers['message']
 def call():
     ret = """
-    echo "MAIL $to $subject $message\\n";
-    return TRUE;
+    \treturn TRUE;
     """
     return ret
+print call()
