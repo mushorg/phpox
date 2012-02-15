4c165bc4e13f33324754f2458d095f415971aec1
Lukas Rist <glaslos@gmail.com>
2012-02-15 17:34:25 +0000

    meta sandbox fixed

--- sandbox.py
+++ sandbox.py
@@ -2,10 +2,12 @@ import sys
 
 import sink.hpf_sink as hpf_sink
 
+
 def main():
-    sink = hpf_sink.HPFeedsSink()
-    sink.run()
-    
+    hpf_sink.run()
+
 if __name__ == "__main__":
-    try: sys.exit(main())
-    except KeyboardInterrupt:sys.exit(0)
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        sys.exit(0)
