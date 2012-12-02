815bdbb3fcfb6ce48d410888b27f2d4940febe61
glaslos <glaslos@gmail.com>
2012-12-02 23:23:53 +0100

    new hpfeeds version

--- hpfeed/hpfeeds.py
+++ hpfeed/hpfeeds.py
@@ -1,7 +1,9 @@
+import sys
 import struct
 import socket
 import hashlib
 import logging
+import time
 
 logger = logging.getLogger('pyhpfeeds')
 
@@ -13,6 +15,25 @@ OP_SUBSCRIBE = 4
 BUFSIZ = 16384
 
 __all__ = ["new", "FeedException"]
+
+
+def msghdr(op, data):
+    return struct.pack('!iB', 5 + len(data), op) + data
+
+
+def msgpublish(ident, chan, data):
+#    if isinstance(data, str):
+#        data = data.encode('latin1')
+    return msghdr(OP_PUBLISH, struct.pack('!B', len(ident)) + ident + struct.pack('!B', len(chan)) + chan + data)
+
+
+def msgsubscribe(ident, chan):
+    return msghdr(OP_SUBSCRIBE, struct.pack('!B', len(ident)) + ident + chan)
+
+
+def msgauth(rand, ident, secret):
+    hash_sum = hashlib.sha1(rand + secret).digest()
+    return msghdr(OP_AUTH, struct.pack('!B', len(ident)) + ident + hash_sum)
 
 
 class FeedUnpack(object):
@@ -35,6 +56,7 @@ class FeedUnpack(object):
         ml, opcode = struct.unpack('!iB', buffer(self.buf, 0, 5))
         if len(self.buf) < ml:
             raise StopIteration('No message.')
+
         data = bytearray(buffer(self.buf, 5, ml - 5))
         del self.buf[:ml]
         return opcode, data
@@ -45,34 +67,27 @@ class FeedException(Exception):
 
 
 class HPC(object):
-    def __init__(self, host, port, ident, secret, timeout=3):
+    def __init__(self, host, port, ident, secret, timeout=3, reconnect=True, sleepwait=20):
         self.host, self.port = host, port
         self.ident, self.secret = ident, secret
         self.timeout = timeout
+        self.reconnect = reconnect
+        self.sleepwait = sleepwait
         self.brokername = 'unknown'
         self.connected = False
         self.stopped = False
         self.unpacker = FeedUnpack()
 
-        self.connect()
+        self.tryconnect()
 
-    def msghdr(self, op, data):
-        return struct.pack('!iB', 5 + len(data), op) + data
-
-    def msgpublish(self, ident, chan, data):
-        if isinstance(data, str):
-            data = data.encode('latin1')
-        return self.msghdr(OP_PUBLISH, struct.pack('!B', len(ident)) + ident +
-                           struct.pack('!B', len(chan)) + chan + data)
-
-    def msgsubscribe(self, ident, chan):
-        return self.msghdr(OP_SUBSCRIBE, struct.pack('!B', len(ident)) +
-                           ident + chan)
-
-    def msgauth(self, rand, ident, secret):
-        hash = hashlib.sha1(rand + secret).digest()
-        return self.msghdr(OP_AUTH, struct.pack('!B', len(ident)) +
-                           ident + hash)
+    def tryconnect(self):
+        while True:
+            try:
+                self.connect()
+                break
+            except FeedException, e:
+                logger.warn('FeedException while connecting: {0}'.format(e))
+                time.sleep(self.sleepwait)
 
     def connect(self):
         logger.info('connecting to {0}:{1}'.format(self.host, self.port))
@@ -98,12 +113,17 @@ class HPC(object):
 
                 logger.debug('info message name: {0}, rand: {1}'.format(name, repr(rand)))
                 self.brokername = name
-                self.s.send(self.msgauth(rand, self.ident, self.secret))
+
+                self.s.send(msgauth(rand, self.ident, self.secret))
                 break
             else:
                 raise FeedException('Expected info message at this point.')
 
         self.s.settimeout(None)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+
+        if sys.platform in ('linux2', ):
+            self.s.setsockopt(socket.SOL_TCP, socket.TCP_KEEPIDLE, 60)
 
     def run(self, message_callback, error_callback):
         while not self.stopped:
@@ -117,8 +137,8 @@ class HPC(object):
                 for opcode, data in self.unpacker:
                     if opcode == OP_PUBLISH:
                         rest = buffer(data, 0)
-                        ident, rest = rest[1:1+ord(rest[0])], buffer(rest, 1+ord(rest[0]))
-                        chan, content = rest[1:1+ord(rest[0])], buffer(rest, 1+ord(rest[0]))
+                        ident, rest = rest[1:1 + ord(rest[0])], buffer(rest, 1 + ord(rest[0]))
+                        chan, content = rest[1:1 + ord(rest[0])], buffer(rest, 1 + ord(rest[0]))
 
                         message_callback(str(ident), str(chan), content)
                     elif opcode == OP_ERROR:
@@ -126,20 +146,22 @@ class HPC(object):
 
                 if self.stopped:
                     break
+
             if self.stopped:
                 break
+            self.tryconnect()
 
     def subscribe(self, chaninfo):
         if type(chaninfo) == str:
             chaninfo = [chaninfo, ]
         for c in chaninfo:
-            self.s.send(self.msgsubscribe(self.ident, c))
+            self.s.send(msgsubscribe(self.ident, c))
 
     def publish(self, chaninfo, data):
         if type(chaninfo) == str:
             chaninfo = [chaninfo, ]
         for c in chaninfo:
-            self.s.send(self.msgpublish(self.ident, c, data))
+            self.s.send(msgpublish(self.ident, c, data))
 
     def stop(self):
         self.stopped = True
@@ -151,7 +173,6 @@ class HPC(object):
             logger.warn('Socket exception when closing.')
 
 
-def new(host=None, port=10000, ident=None, secret=None):
-    if not host or not ident or not secret:
-        raise Exception('host, ident and secret are mandatory!')
-    return HPC(host, port, ident, secret)
+def new(host=None, port=10000, ident=None, secret=None,
+        timeout=3, reconnect=True, sleepwait=20):
+    return HPC(host, port, ident, secret, timeout, reconnect)
