74f5c0c0ff3d53ba17b767dc92285245c52fd66c
glaslos <glaslos@gmail.com>
2015-12-31 17:42:31 +0100

    switch over to python3

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright (C) 2015 Lukas Rist
 #
@@ -17,31 +17,19 @@
 # Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from gevent.monkey import patch_all
-patch_all()
-
 import os
-
-import gevent
-import gevent.timeout
-import gevent.subprocess
+import asyncio
+from asyncio.subprocess import PIPE
 
 from pprint import pprint
-from ConfigParser import ConfigParser
 
 import analysis
-import listener
 
 
 class PHPSandbox(object):
 
     def __init__(self, pre=os.getcwd() + '/', debug_level=0):
         self.pre = pre
-        self.conf_parser = ConfigParser()
-        self.conf_parser.read(self.pre + "sandbox.cfg")
-        self.DEBUG_LEVEL = debug_level
-        self.greenlet = None
-        self.fake_listener = None
 
     @classmethod
     def php_tag_check(cls, script):
@@ -54,58 +42,47 @@ class PHPSandbox(object):
             check_file.write(file_content)
         return script
 
-    def sandbox(self, script, secs):
+    @asyncio.coroutine
+    def sandbox(self, script):
         if not os.path.isfile(script):
             raise Exception("Sample not found: {0}".format(script))
-        self.fake_listener = listener.FakeListener()
-        self.greenlet = self.fake_listener.run()
+        # self.fake_listener = listener.FakeListener()
+        # self.greenlet = self.fake_listener.run()
         try:
-            proc_sandbox = gevent.subprocess.Popen(
-                [
-                    "php5",
-                    self.pre + "sandbox.php", script
-                ],
-                stdout=gevent.subprocess.PIPE,
-                stderr=gevent.subprocess.PIPE,
-                shell=False
-            )
+            cmd = [
+                "php5",
+                self.pre + "sandbox.php", script
+            ]
+            self.proc = yield from asyncio.create_subprocess_exec(*cmd, stdout=PIPE)
+            stdout_value = b''
+            while True:
+                line = yield from self.proc.stdout.readline()
+                print(line)
+                if not line:
+                    break
+                else:
+                    stdout_value += line
         except Exception as e:
-            proc_sandbox = None
-            print("Error executing the sandbox:", e.message)
-        else:
-            if self.DEBUG_LEVEL > 0:
-                print("Sandbox running...")
-        try:
-            with gevent.Timeout(secs):
-                stdout_value = ""
-                while True:
-                    try:
-                        stdout_value += proc_sandbox.stdout.readline()
-                    except gevent.timeout.Timeout:
-                        break
-                    gevent.sleep(0.1)
-            proc_sandbox.kill()
-        except Exception as e:
-            self.greenlet.kill()
-            print("Communication error:", e.message)
-        else:
-            self.greenlet.kill()
-            analyzer = analysis.DataAnalysis(script, debug=self.DEBUG_LEVEL)
-            botnet = analyzer.analyze(stdout_value)
-            if self.DEBUG_LEVEL > 0:
-                print("Parsed with sandbox")
-                pprint(botnet.todict())
-            return botnet
+            try:
+                self.proc.kill()
+            except Exception:
+                pass
+            print("Error executing the sandbox: {}".format(e))
+            # raise e
+
+        print("Sandbox running...")
+        analyzer = analysis.DataAnalysis(script)
+        botnet = analyzer.analyze(stdout_value)
+        print("Parsed with sandbox")
+        pprint(botnet.todict())
+        return botnet
 
 if __name__ == '__main__':
     DEBUG_LEVEL = 1
     sb = PHPSandbox(debug_level=DEBUG_LEVEL)
+    loop = asyncio.get_event_loop()
     try:
-        sb.sandbox('samples/irc_bot.php', secs=10)
-    except IndexError:
-        print("Specify the file to analyze...")
-        raise
-    except:
-        if sb.greenlet:
-            sb.greenlet.kill()
-        raise
+        loop.run_until_complete(sb.sandbox('bot.php'))
+    except KeyboardInterrupt:
+        pass
+    loop.close()
